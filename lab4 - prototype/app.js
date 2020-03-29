08bec158ec43adba0260cd8e484050432ccc7241
TERRAKID <wannes.verboven@outlook.com>
2020-03-29 16:35:31 +0200

    [feat] Lab 4: Add holiday message

--- lab4 - prototype/app.js
+++ lab4 - prototype/app.js
@@ -1,5 +1,11 @@
 class App {
     constructor() {
+        if (sessionStorage.holiday == null) {
+            this.year;
+            this.month;
+            this.day;
+            this.getDate();
+        }
         if (!((sessionStorage.raining === "true") || (sessionStorage.raining === "false"))) {
             this.lat;
             this.lng;
@@ -45,6 +51,9 @@ class App {
     }
 
     setText() {
+        if (sessionStorage.holiday != "false") {
+            document.querySelector(".holiday").innerHTML = "Happy, " + sessionStorage.holiday;
+        }
         if (sessionStorage.raining === "true") {
             document.querySelector(".slogan-1").innerHTML = "Regent het buiten?";
             document.querySelector(".slogan-2").innerHTML = "Ga naar de zon!";
@@ -54,6 +63,30 @@ class App {
         }
     }
 
+    getDate() {
+        let date =  new Date();
+        this.year = date.getFullYear();
+        this.month = date.getMonth() + 1;
+        this.day = date.getDate();
+        this.getHoliyday();
+    }
+
+    getHoliyday() {
+        let url = `https://calendarific.com/api/v2/holidays?&api_key=97a344558a981a04adcea3a53bcc3be00c415a2f&country=BE&year=${this.year}&month=${this.month}&day=${this.day}&language=nl&type=national`;
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            if (data.response.holidays.length > 0) {
+                sessionStorage.holiday = data.response.holidays[0].name;
+            } else {
+                sessionStorage.holiday = false;
+            }
+            this.setText();
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+   
     errorLocation(err) {
         console.log(err);
     }
